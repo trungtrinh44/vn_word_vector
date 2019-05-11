7a68f0bf4bf4b7fe7a451f3d8babf82a0e31b1ca
Trinh Quoc Trung <trinhtrung96@gmail.com>
2019-05-11 20:27:54 +0700

    clean text version 2.1

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,6 @@ import regex
 def clean_text(text):
     text = unicodedata.normalize('NFKC', text)
     text = regex.sub(r"[“”]", '"', text)
-    text = regex.sub(r"([[:punct:]]+)", r" \g<1> ", text)
+    text = regex.sub(r"([[:punct:]])", r" \g<1> ", text)
     text = regex.sub(r"\s+", " ", text)
     return text
