75fcf76d9bbb3988c6ab4daedeca5ac60778b09e
Trinh Quoc Trung <trinhtrung96@gmail.com>
2019-05-11 21:46:07 +0700

    allow lower

--- preprocess.py
+++ preprocess.py
@@ -2,9 +2,11 @@ import unicodedata
 import regex
 
 
-def clean_text(text):
+def clean_text(text, lower=False):
     text = unicodedata.normalize('NFKC', text)
     text = regex.sub(r"[“”]", '"', text)
     text = regex.sub(r"([[:punct:]])", r" \g<1> ", text)
     text = regex.sub(r"\s+", " ", text)
+    if lower:
+        return text.lower()
     return text
